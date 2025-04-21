a1c870cb003445aa56b07c8f2431052eca747518
nurlybek <orozovnurlybek@gmail.com>
2025-04-21 16:19:20 +0600

    Добавил конфиг и настроил сервер

---
+++ index.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import config from "./config";
+import usersRouter from "./routers/users";
+import tasksRouter from "./routers/tasks";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+app.use('/users', usersRouter)
+app.use('/tasks', tasksRouter)
+
+
+const run = async () => {
+    await mongoose.connect(config.db);
+
+    app.listen(port, () => {
+        console.log(`Server started on http://localhost:${port}`);
+    })
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    })
+}
+run().catch(console.error);
